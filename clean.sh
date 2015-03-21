d1eca96e965473eb64c233826aea81ca412ec2b4
Erik Nelson <enelson@cmu.edu>
2015-03-21 13:38:55 -0400

    Adding chapters and sections (full outline)

--- clean.sh
+++ clean.sh
@@ -8,6 +8,7 @@ rm *.toc > /dev/null 2>&1
 rm *.lof > /dev/null 2>&1
 rm *.log > /dev/null 2>&1
 rm *.lot > /dev/null 2>&1
+rm *.out> /dev/null 2>&1
 cd ./content > /dev/null 2>&1
 rm *.aux > /dev/null 2>&1
 cd - > /dev/null 2>&1
